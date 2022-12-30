b6dafaa2ade7b1fc3eb6cbbc3cc9b3eccdcb21f7
cleverAkanimoh <crushclever1@gmail.com>
2022-12-30 10:14:14 +0100

    added 20 files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Components/Header';
+import MainContent from './Components/MainContent';
+import Data from './Components/Data';
+
+const App = () => {
+
+  return (
+    <main>
+      <Header />
+      <MainContent />
+    </main>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import world from '../images/world.svg'
+
+const Header = () => {
+    return ( 
+        <header>
+            <img src={world} alt="World" />
+        </header>
+     );
+}
+ 
+export default Header;
