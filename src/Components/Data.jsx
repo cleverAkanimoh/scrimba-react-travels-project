2d73c7aaddee32b12a78a34b9df0603a52931bbc
cleverAkanimoh <crushclever1@gmail.com>
2022-12-30 11:00:47 +0100

    modified Data.jsx in Components

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,5 +1,29 @@
 export default [
     {
         id:1,
-    }
+        img: '/images/Rectangle77.png',
+        title: 'Mount Fuji',
+        country: 'JAPAN',
+        href: '#',
+        date: '12 Jan, 2021 - 24 Jan, 2021',
+        desc: `Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.`
+    },
+    {
+        id:2,
+        img: '/images/Rectangle78.png',
+        title: 'Mount Fuji',
+        country: 'JAPAN',
+        href: '#',
+        date: '12 Jan, 2021 - 24 Jan, 2021',
+        desc: `Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.`
+    },
+    {
+        id:3,
+        img: '/images/Rectangle84.png',
+        title: 'Mount Fuji',
+        country: 'JAPAN',
+        href: '#',
+        date: '12 Jan, 2021 - 24 Jan, 2021',
+        desc: `Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.`
+    },
 ]
